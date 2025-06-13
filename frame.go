36b9ef68e574713c7af96c5836c6551e78f5cc87
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2025-06-13 15:10:09 +0700

    feat: update frame

--- frame.go
+++ frame.go
@@ -4,22 +4,26 @@ import (
 	"fmt"
 )
 
+type Test struct {
+	word1 string
+	word2 string
+	want  string
+}
+
 func main() {
-	tests := []struct {
-		nums   []int
-		target int
-		want   []int
-	}{
-		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
-		{[]int{3, 2, 4}, 6, []int{1, 2}},
-	}
+	tests := []Test{}
+	tests = append(tests, Test{"abc", "pqr", "apbqcr"})
+	tests = append(tests, Test{"ab", "pqrs", "apbqrs"})
+	tests = append(tests, Test{"abcd", "pq", "apbqcd"})
 
 	for _, tc := range tests {
-		got := thefunction(tc.nums, tc.target)
-		fmt.Printf("Input: %v, %d => Output: %v, Expected: %v\n", tc.nums, tc.target, got, tc.want)
+		got := mergeAlternately(tc.word1, tc.word2)
+		if got != tc.want {
+			fmt.Printf("Input: %s, %s => Output: %s, Expected: %s\n", tc.word1, tc.word2, got, tc.want)
+		}
 	}
 }
 
-func thefunction(nums []int, target int) []int {
-	return []int{0, 1} // Placeholder implementation
+func mergeAlternately(word1 string, word2 string) string {
+	return "abc"
 }
